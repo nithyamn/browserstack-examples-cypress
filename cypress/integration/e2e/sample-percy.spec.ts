5222d281c87adf3eff75b679031f72faecc20177
Nithya Mani <nithya.mn999@gmail.com>
2023-01-10 20:35:54 +0530

    updated test command

--- cypress/integration/e2e/sample-percy.spec.ts
+++ cypress/integration/e2e/sample-percy.spec.ts
@@ -2,17 +2,15 @@ import '@percy/cypress'
 
 describe('Percy Test', function () {
 
- it('Localhost website - About us', () => {
+ it('Bynder - Login Page', () => {
     var widthArray = [375, 768, 1280];
-    cy.visit('http://localhost:3000/');
-    cy.percySnapshot('About Us Page'); 
+    
     //cy.percySnapshot('About Us Page', widths: [375, 768, 1280]); 
     //cy.percySnapshot('About Us Page',{percyCSS: `.change-font{display: none;} .fixed-header{display: none;}`}); 
   })
 
-  it('Localhost website - Contact us', () => {
-    cy.visit('http://localhost:3000/');
-    cy.percySnapshot('Contact Us Page'); 
+  it('Bynder - Home Page', () => {
+    cy.percySnapshot('Home Page');
   })
 });
 
